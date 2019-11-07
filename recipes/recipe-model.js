a364213793ae535a0ae703b0292d0fb8dac463c8
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-11-07 21:14:26 +0100

    Get shopping list endpoint completed

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -2,15 +2,40 @@ const knex = require("knex");
 const db = require("../data/db-config");
 
 module.exports = {
-  getRecipes
-  //   getShoppingList,
+  getRecipes,
+  getShoppingList
   //   getInstructions
 };
 
 function getRecipes() {
-  return db("recipes as re");
+  return db("recipes");
 }
 
+// function getShoppingList(recipe_id) {
+//   return db("recipe_book")
+//     .where({ recipe_id })
+//     .first();
+// }
+
+/**
+ * 
+ *
+select * from recipe_book
+join recipes
+on recipe_book.recipe_id = recipes.id
+join ingredients
+on recipe_book.ingredient_id = ingredients.id
+where recipe_book.recipe_id = 2;
+ */
+
 function getShoppingList(recipe_id) {
-    
+  return db("recipe_book")
+    .join("recipes", "recipe_book.recipe_id", "recipes.id")
+    .join("ingredients", "recipe_book.ingredient_id", "ingredients.id")
+    .select(
+      "recipe_book.quantity",
+      "recipes.recipe_name",
+      "ingredients.ingredient_name"
+    )
+    .where({ recipe_id });
 }
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -17,4 +17,22 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Recipes.getShoppingList(id)
+    .then(recipe => {
+      if (recipe) {
+        res.json(recipe);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find recipe with given id." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get recipes" });
+    });
+});
+
 module.exports = router;
